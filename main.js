94499df0324477dce5da18180cf5be334beb50a3
Eugénie Roques <eugenieroques@Eugenies-MacBook-Air.local>
2018-06-14 12:35:39 +0200

    fixes feature #2

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-var stt = document.querySelector('.scroll-to-top');
-stt.addEventListener('click', function() {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-})
+ // SCROLL TO TOP
+
+ var stt = document.querySelector('.scroll-to-top');
+ stt.addEventListener('click', function() {
+   window.scroll({
+     top: 0,
+     left: 0,
+     behavior: 'smooth'
+   });
+ })
+
+ // NAV Bar
+ var headerNav = document.querySelector('.nav-scroll');
+
+ document.addEventListener('wheel', function(event) {
+   if (event.deltaY > 0) {
+     headerNav.style.marginTop = "-140px";
+   } else if (event.deltaY < 0) {
+     headerNav.style.marginTop = "";
+   }
+ });
